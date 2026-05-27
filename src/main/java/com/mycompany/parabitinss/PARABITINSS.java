87f4ca58418d050e9dc6d6e33708088b8c5adb71
Arvind Yadav <152893706+arvindyadav2028@users.noreply.github.com>
2026-05-27 07:09:52 +0100

    Initial commit

---
+++ src/main/java/com/mycompany/parabitinss/PARABITINSS.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.parabitinss;
+
+import java.awt.BorderLayout;
+
+/**
+ *
+ * @author arvin
+ */
+public class PARABITINSS {
+
+    public static void main(String args[]) {
+//        java.awt.EventQueue.invokeLater(new Runnable() {
+//            public void run() {
+//                PbtEmpLog dialog = new PbtEmpLog(new javax.swing.JFrame(), true);
+//                dialog.setLayout(new BorderLayout());
+//                dialog.addWindowListener(new java.awt.event.WindowAdapter() {
+//                    @Override
+//                    public void windowClosing(java.awt.event.WindowEvent e) {
+//                        System.exit(0);
+//                    }
+//                });
+//                dialog.setVisible(true);
+//            }
+//        });
+    }
+}
